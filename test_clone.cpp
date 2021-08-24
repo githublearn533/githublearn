26d500a7ee684d4245efecfbcf155163cb8bcfcd
sandy533533 <89172562+sandy533533@users.noreply.github.com>
2021-08-24 14:54:14 +0800

    Update test_clone.cpp

--- test_clone.cpp
+++ test_clone.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std ;
+//main
+int main ()
+{
+    cout << "hello, sandy! \n";
+    cout << "hello, sandy! \n";
+    cout << "hello, sandy! \n";
+    cout << "hello, sandy! \n";
+    cout << "hello, sandy! \n";
+    cout << "hello, sandy! \n";
+    cout << "hello, sandy! \n";
 
+
+
+    return 0;
+
+}
